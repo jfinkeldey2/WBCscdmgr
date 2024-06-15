24b9a252a82728f074222a3f2e4c9e7f61c49280
Jeff Finkeldey <jfinkeldey@aol.com>
2024-06-15 19:26:53 -0400

    Initial 2024 version after schedule loaded

--- app/src/main/java/org/boardgamers/wbcscdmgr/MainActivity.java
+++ app/src/main/java/org/boardgamers/wbcscdmgr/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 			Toast.makeText(this,
 					"ERROR: Could not find version code, contact " + getResources().getString(R.string.email) +
 							" for help.", Toast.LENGTH_LONG).show();
-			currentVersion = 20230108; // TODO set as version code here and android manifest and gradle build
+			currentVersion = 20240101; // TODO set as version code here and android manifest and gradle build
 			e.printStackTrace();
 		}
 
--- app/src/main/java/org/boardgamers/wbcscdmgr/SplashScreen.java
+++ app/src/main/java/org/boardgamers/wbcscdmgr/SplashScreen.java
@@ -147,7 +147,7 @@ public class SplashScreen extends AppCompatActivity {
 			// find schedule file
 			InputStream is;
 			try {
-				is = getAssets().open("schedule2023.txt");
+				is = getAssets().open("schedule2024.txt");
 			} catch (IOException e2) {
 				e2.printStackTrace();
 				return -2;
