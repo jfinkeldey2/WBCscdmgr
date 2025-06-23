6dfea49be635ae224ab02a43b094c8cb613900ad
Jeff Finkeldey <jfinkeldey@aol.com>
2025-06-23 16:00:56 -0400

    Version 2025.108 updated static number of events

--- app/src/main/java/org/boardgamers/wbcscdmgr/SplashScreen.java
+++ app/src/main/java/org/boardgamers/wbcscdmgr/SplashScreen.java
@@ -30,7 +30,7 @@ public class SplashScreen extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 
 	//	int TOTAL_EVENTS = 751;
-		int TOTAL_EVENTS = 1077;
+		int TOTAL_EVENTS = 1071;
 
 		WBCDataDbHelper dbHelper = new WBCDataDbHelper(this);
 		dbHelper.onUpgrade(dbHelper.getWritableDatabase(), dbHelper.getVersion(),
